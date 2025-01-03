01d18db391588abfcc8b5bcf407f3956c1db2749
andresengler <141840530+andresengler@users.noreply.github.com>
2025-01-03 20:57:36 -0300

    Update next.config.js with optimized settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['placeholder.com'], // Añade aquí los dominios de las imágenes externas que uses
+  },
 }
 
 module.exports = nextConfig
-
